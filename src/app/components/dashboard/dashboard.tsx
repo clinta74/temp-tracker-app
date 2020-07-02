5d53a5c692844951d00e8094e6585869fa13afa2
candrews <clinta74@gmail.com>
2020-07-02 19:47:50 -0400

    user updates

--- src/app/components/dashboard/dashboard.tsx
+++ src/app/components/dashboard/dashboard.tsx
@@ -4,7 +4,7 @@ export const Dashboard: React.FunctionComponent = () => {
     return (
         <div className="p-4 text-center">
             <h2>Temp Tracker Dashboard</h2>
-            <iframe src="https://xeams.pollyspeople.net/graph" style={{ border: 'none', width: '410px', height: '420px' }} ></iframe>
+            <iframe src="https://temptracker.pollyspeople.net/graph" style={{ border: 'none', width: '100%', height: '420px' }} ></iframe>
         </div>
     )
 }
